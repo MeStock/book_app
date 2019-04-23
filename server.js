06be2841d6ea2de6bc63eb90c54358eeaa305f7b
mestock <MelissaStock44@gmail.com>
2019-04-23 11:37:48 -0700

    Completed function, added route to /searches, gathered data from googleAPI

--- server.js
+++ server.js
@@ -20,7 +20,31 @@ app.get('*',(request, response)=>{
   response.render('pages/index.ejs', {root:'./views'});
   
 })
+//----------------GLOBAL CONSTANTS-------------------------
 
+
+//--------------CONSTRUCTOR FUNCTION-----------------------
+
+function BookConstructor(title, author, description){
+  this.title = title;
+  this.author = author;
+  this.description = description;
+}
+
+app.post('/searches', (request, response) => {
+  // response.send(console.log(request.body.search[0]));
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${request.body.search[0]}`;
+  superagent.get(URL).then(result => {
+    let bookReturn = result.body.items;
+    let tenBooks = bookReturn.map(bookArray => {
+      let bookData = result.body.items[0].volumeInfo;
+      let bookObj = new BookConstructor(bookData.title, bookData.authors[0], bookData.description);
+      return bookObj;
+    });
+    // console.log(tenBooks);
+    response.render('pages/searches/show.ejs');
+  });
+})
 
 
 
