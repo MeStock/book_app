fa9167f94c2440869066b03667fd38d041ce5718
mestock <MelissaStock44@gmail.com>
2019-04-23 12:02:18 -0700

    Completed tues-feature3

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ function BookConstructor(title, author, description){
 
 app.post('/searches', (request, response) => {
   // response.send(console.log(request.body.search[0]));
-  const URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${request.body.search[0]}`;
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=+title:${request.body.search[0]}`;
   superagent.get(URL).then(result => {
     let bookReturn = result.body.items;
-    let tenBooks = bookReturn.map(bookArray => {
-      let bookData = result.body.items[0].volumeInfo;
-      let bookObj = new BookConstructor(bookData.title, bookData.authors[0], bookData.description);
+    let tenBooks = bookReturn.map((bookArray, idx) => {
+      let bookData = result.body.items[idx].volumeInfo;
+      let bookObj = new BookConstructor(bookData.title, bookData.authors, bookData.description);
       return bookObj;
     });
     // console.log(tenBooks);
