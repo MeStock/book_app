749ebe09dca5f5fbf6bb35d897493cd32414bdc9
mestock <MelissaStock44@gmail.com>
2019-04-25 11:50:58 -0700

    Completed lab12 feature2: added view detail button

--- data/books.sql
+++ data/books.sql
@@ -8,9 +8,7 @@ id SERIAL PRIMARY KEY,
 title VARCHAR(255),
 author VARCHAR(255),
 description VARCHAR(1020),
--- isbn VARCHAR(255),
+isbn VARCHAR(255),
 image_url VARCHAR(255),
 bookshelf VARCHAR(255)
 );
-INSERT INTO books (title, author, description, image_url, bookshelf) VALUES( 'Harry', 'rowling', 'wizard', 'image', 'bookshelf');
-INSERT INTO books (title, author, description, image_url, bookshelf) VALUES( 'Harry', 'rowling', 'wizard', 'image', 'bookshelf');
