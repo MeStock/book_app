c8b0afa3213acdd45b0e6c2c0e9d06219917f304
mestock <MelissaStock44@gmail.com>
2019-04-25 00:34:50 -0700

    Compelted feature #1, refactored each GET into individual functions, added form to seaches page to add save function

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const superagent = require ('superagent');
 const app = express();
+const pg = require('pg');
 
 const PORT = process.env.PORT || 3000;
-
-const pg = require('pg');
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', console.error);
@@ -18,70 +18,68 @@ app.use(express.urlencoded({extended:true}));
 app.set('view-engine', 'ejs');
 
 
+app.get('/', displayDataFromDB);
+app.get('*',displayDataFromDB);
 
-app.get('/', addToDB );
-
-
-
-app.get('/new', (request,response)=>{
-response.render('pages/searches/new.ejs');
-})
-app.get('*',(request, response)=>{
-  response.render('pages/index.ejs', {root:'./views'});
-})
-
+app.get('/new', newSearch);
 app.post('/searches', getData);
+
+//TODO: SEE FEATURE TWO (LAB 12)
+// app.get('/books/:id', );
+
+app.post('/saved_books', addBooksToDB);
 
 //app.post('/', addToDB);
 
 //----------------GLOBAL CONSTANTS-------------------------
 
-
+//---------------------------------------------------------
 //--------------CONSTRUCTOR FUNCTION-----------------------
+//---------------------------------------------------------
 
 function BookConstructor(bookObj){
-  this.title = bookObj.title;
-  this.authors = bookObj.author;
-  this.description = bookObj.description;
-  this.isbn = bookObj.industryIdentifiers[0].identifier;
-  //this.image_url = bookObj.imageLinks.smallThumbnail;
-  this.bookshelf = "test";
+  this.title = bookObj.volumeInfo.title;
+  this.authors = bookObj.volumeInfo.authors;
+  this.description = bookObj.volumeInfo.description;
+  this.isbn = bookObj.volumeInfo.industryIdentifiers ? bookObj.volumeInfo.industryIdentifiers[0].identifier : 'No isbn available.';
+  this.image_url = bookObj.volumeInfo.imageLinks ? bookObj.volumeInfo.imageLinks.thumbnail.replace('http:', 'https:') : 'noimage.jpeg';
+  this.bookshelf = 'TBD';
 }
 
-  
+//---------------------------------------------------------
+//--------------------OTHER FUNCTIONS-----------------------
+//----------------------------------------------------------
+
 function getData(request, response){
-const URL = `https://www.googleapis.com/books/v1/volumes?q=+title:${request.body.search[0]}`;
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=+title:${request.body.search[0]}`;
   superagent.get(URL).then(result => {
     if(result.body.totalItems === 0) {
       response.status(500).send('Sorry, something went wrong');
       return;
     }
     let bookReturn = result.body.items;
-    let tenBooks = bookReturn.map((bookArray, idx) => {
-    let bookData = result.body.items[idx].volumeInfo;
-    let bookObj = new BookConstructor(bookData);
-      //client.query('INSERT INTO books(title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6)', [bookObj.title, bookObj.author, bookObj.description, bookObj.isbn, bookObj.image_url, bookObj.bookshelf ])
-    return bookObj;
+    let tenBooks = bookReturn.map(bookArray => {
+      return new BookConstructor(bookArray);
     });
-  
     response.render('pages/searches/show.ejs', {tenBooks});
   });
 }
 
-function addToDB(request,response){
-  
+function displayDataFromDB(request,response){
   client.query('SELECT * FROM books;').then(result =>{
-  console.log(result.rows);
-  response.render('pages/index.ejs', {tenBooks: result.rows});
+    response.render('pages/index.ejs', {tenBooks: result.rows});
   })
-
 }
-function frontPageDisplay(request,response){
-  console.log(request.body)
-  response.render('pages/index.ejs',{});
 
-
-
-
+function newSearch(request, response){
+  response.render('pages/searches/new.ejs');
 }
+
+function addBooksToDB(request, response){
+  const {title, author, description, isbn, image_url, bookshelf } = request.body;
+  client.query('INSERT INTO books(title, author, description, isbn, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6);', [title, author, description, isbn, image_url, bookshelf]).then(() => {
+    response.redirect('/');
+  })
+}
+
 app.listen(PORT, () => console.log(`app is up on port ${PORT}`));
